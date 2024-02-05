4a7e0a35f2806cd0dc87f3b1502ebd828a0279c5
its-sky <smc9919@naver.com>
2024-02-05 23:48:29 +0900

    feat: Singleton pattern 예제

---
+++ src/main/java/singleton/App.java
@@ -0,0 +1,45 @@
+package singleton;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class App {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Settings settings = Settings.getInstance();
+        Settings settings1 = Settings.getInstance();
+        System.out.println(settings == settings1);
+
+        /**
+         * java reflection을 통해 singleton 보장을 깨뜨릴 수 있음.
+         * 굳이 막자면 Settings를 enumeration을 사용하면 됨. 리플렉션에 안전함.
+         * 단점은 클래스를 로드하는 순간 이미 만들어지는게 단점이라면 단점임.
+         */
+        /*Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings2 = constructor.newInstance();*/
+
+        /**
+         * 직렬화/역질렬화를 통해 singleton을 깨뜨릴 수 있음.
+         * Serializable 구체 클래스에 protected Object readResolve() 시그니처 메소드가 있으면 역직렬화를 할 때 무조건 거치게 되어 있어서
+         * 해당 방법을 막을 수 있음.
+         */
+        /*try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings);
+        }
+
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            Settings settings2 = (Settings) in.readObject();
+        }*/
+
+    }
+
+}
---
+++ src/main/java/singleton/Settings.java
@@ -0,0 +1,62 @@
+package singleton;
+
+import java.io.Serializable;
+
+public class Settings implements Serializable {
+
+    private static Settings instance;
+
+    private Settings() {}
+
+    /**
+     * getInstance가 호출될 때 SettingsHolder에 INSTANCE가 lazy laading이 되고
+     * 복잡도가 낮아짐. (권장되는 방법1)
+     */
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
+    public static Settings getInstance() {
+        return SettingsHolder.INSTANCE;
+    }
+
+    protected Object readResolve() {
+        return getInstance();
+    }
+
+    /**
+     * private static final Settings INSTANCE = new Settings();
+     * 생성 비용이 낮다면 eager initialization으로 instance에 생성해서 처음에 주입하는 방식도 사용할 수 있음. (Thread-safe)
+     * 미리 만든다는 것 자체가 단점이 될 수 있음. 인스턴스 생성 과정이 길고 메모리를 많이 사용하고 애플리케이션에서 많이 쓰지 않으면
+     * 손해가 발생할 수 있음.
+     */
+
+    /**
+     * 가장 쉽게 thread-safe한 싱글톤 구현하기
+     * 단점은 getInstance를 호출할 때마다 동기화 blocking 처리 때문에 성능이 하락할 수 있음.
+     */
+    /*public static synchronized Settings getInstance() {
+        if (instance == null) {
+            instance = new Settings();
+        }
+        return instance;
+    }*/
+
+    /**
+     * double checked locking
+     * 대신 instance 앞에 키워드에 volatile이라고 써줘야함. (Java 1.5 이후)
+     */
+    /*public static Settings getInstance() {
+        if (instance == null) {
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+
+        return instance;
+    }*/
+
+
+}
