a6953a5469512fdc74cb677e29c4319ebcbbb356
Mincheol Shin <48898994+its-sky@users.noreply.github.com>
2024-02-08 18:30:19 +0900

    remove: 디렉토리 파일 삭제

--- src/main/java/creational_patterns/factorymethod/BlackshipFactory.java
+++
@@ -1,9 +0,0 @@
-package creational_patterns.factorymethod;
-
-public class BlackshipFactory extends DefaultShipFactory {
-
-    @Override
-    public Ship createShip() {
-        return new Blackship();
-    }
-}
