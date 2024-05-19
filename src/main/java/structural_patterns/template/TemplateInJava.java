267a529568b8f49463165a79ec7701de68d784a6
its-sky <smc9919@naver.com>
2024-05-19 16:10:45 +0900

    feat: Template Callback Pattern Java 예제 추가

---
+++ src/main/java/structural_patterns/template/TemplateInJava.java
@@ -0,0 +1,20 @@
+package structural_patterns.template;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TemplateInJava extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
